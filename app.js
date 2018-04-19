7f12fbdd9a9e4ee79231b828dee17e19662128da
luka dodelia <luka@picktek.com>
2018-04-19 16:43:38 +0400

    manual login

--- app.js
+++ app.js
@@ -51,7 +51,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.post('/login', (req, res, next) => {
-  req.login(req.body.username, function (err) {
+  let user;
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (users.hasOwnProperty(username) && users[username].password === password) {
+    user = users[username];
+  } else {
+    return next('Incorrect username or password.');
+  }
+
+  req.login(user, function (err) {
     if (err) { return next(err); }
 
     res.status(401).json({
